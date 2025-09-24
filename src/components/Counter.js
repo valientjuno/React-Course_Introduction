a2b45700fb4378d0fde0d505d88976acc27d0cb7
valientjuno <jessedoake91@gmail.com>
2025-09-24 13:51:47 -0600

    initial commit for boilerplate

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "./counter.css";
+const Counter = () => {
+  return (
+    <div>
+      <h2> And some more Here</h2>Counter
+    </div>
+  );
+};
+
+export default Counter;
