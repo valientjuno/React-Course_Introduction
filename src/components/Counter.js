bcf2b215fd255a87eeaf298f4b9d97ee9c0325a1
valientjuno <jessedoake91@gmail.com>
2025-09-25 12:10:27 -0600

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
 import "./counter.css";
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
   return (
-    <div>
-      <h2> And some more Here</h2>Counter
+    <div className="container">
+      <h2>And some more Here</h2>
+      <button className="btn btn-3" onClick={handleDecrement}>
+        Minus 1
+      </button>
+      <button className="btn btn-2" onClick={handleIncrement}>
+        Add 1
+      </button>
+      <h3 className="counter">Count is {count}</h3>
+      <button className="btn btn-3" onClick={() => setCount(0)}>
+        Reset Counter
+      </button>
     </div>
   );
 };
