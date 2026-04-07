a2938e346eee0465d3a1b9fd505454d963a9bbfc
valientjuno <jessedoake91@gmail.com>
2026-04-07 10:21:46 -0600

    Fix formatting in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./components/Counter";
+
 function App() {
   return (
     <div>
-      <h1>React Counter</h1> <Counter />
+      <h1>React Counter</h1>
+      <Counter />
     </div>
   );
 }
+
 export default App;
